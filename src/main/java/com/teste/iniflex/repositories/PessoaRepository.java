fc1150b36e28350cf7a89a6b17ec19256e5aba24
gemeoslemes <victorlemes0776@gmail.com>
2024-06-18 13:50:08 -0300

    Criando interfaces de FuncionarioRepository e PessoaRepository.

---
+++ src/main/java/com/teste/iniflex/repositories/PessoaRepository.java
@@ -0,0 +1,9 @@
+package com.teste.iniflex.repositories;
+
+import com.teste.iniflex.model.pessoa.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+}
